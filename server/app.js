62dc938c40a0aef86b913451a83a4685593a68a8
Milan Avić <milan@hivemind.ch>
2019-12-24 10:51:54 +0100

    Filters

--- server/app.js
+++ server/app.js
@@ -21,9 +21,8 @@ app.get('/city', (req, res) => {
 });
 
 app.get('/space/:spaceId', (req, res) => {
-    db.get('SELECT * FROM space s JOIN user u ON s.host_id = u.id WHERE s.id = ?', [req.params.spaceId], (err, row) => {
+    db.get('SELECT * FROM space WHERE space.id = ?', [req.params.spaceId], (err, row) => {
         if (row) {
-            console.log(row.photos);
             res.send(({...row, amenities: JSON.parse(row.amenities), photos: JSON.parse(row.photos)}));
         } else {
             res.status(404).send();
@@ -33,27 +32,40 @@ app.get('/space/:spaceId', (req, res) => {
 
 app.get('/space', (req, res) => {
     if (req.query.filters) {
-        console.log(req.query.filters);
         const filters = JSON.parse(Buffer.from(req.query.filters, 'base64').toString());
         let query = 'SELECT * FROM space';
         const keys = [];
         const values = [];
         Object.keys(filters).forEach((key) => {
-            if (key === 'wifi' && filters[key]) {
-                keys.push('json_extract(space.amenities, "$.wifi") = ?');
-                values.push(filters[key]);
-            }
+
+            ['wifi', 'everyChairHasAComputer', 'toilet', 'soundSystem', 'heatingAc', 'projector'].forEach(k => {
+                if (key === k && filters[key]) {
+                    keys.push(`json_extract(space.amenities, "$.${key}") = ?`);
+                    values.push(filters[key]);
+                }
+            });
 
             if (key === 'city_id') {
                 keys.push('city_id = ?');
                 values.push(filters[key]);
+            }
+
+            if (key === 'priceRange') {
+                keys.push('price BETWEEN ? AND ?');
+                values.push(filters[key][0]);
+                values.push(filters[key][1]);
+            }
+
+            if (key === 'guestsRange') {
+                keys.push('guests BETWEEN ? AND ?');
+                values.push(filters[key][0]);
+                values.push(filters[key][1]);
             }
         });
         if (keys.length) {
             query += ' WHERE ' + keys.join(' AND ');
         }
         db.all(query, values, (err, rows) => {
-            console.log(query, err)
             res.send(rows.map(r => ({
                 ...r,
                 amenities: JSON.parse(r.amenities),
@@ -61,7 +73,7 @@ app.get('/space', (req, res) => {
             })));
         });
     } else {
-        db.all("SELECT * FROM space INNER JOIN user ON space.host_id = user.id", (err, rows) => {
+        db.all("SELECT * FROM space", (err, rows) => {
             const parsedRows = rows.map(r => ({
                 ...r,
                 amenities: JSON.parse(r.amenities),
